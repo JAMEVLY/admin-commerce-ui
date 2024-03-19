1e75c0b8c429fd33fcd19ba1f58d8f3329caf86e
AlejandroCristM <alejandro.cristanchom@gmail.com>
2024-03-18 22:59:47 -0500

    feat: adding header and footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "./Components/Molecules/Header";
+import Footer from "./Components/Molecules/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "JAMEVLY Commerce Adm",
-  description: "This is the admin commerce app to set up your JAMEVLY commerce.",
+  description:
+    "This is the admin commerce app to set up your JAMEVLY commerce.",
 };
 
 export default function RootLayout({
@@ -16,7 +19,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
